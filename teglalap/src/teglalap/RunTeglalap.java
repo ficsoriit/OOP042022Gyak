1357699d5d168bf62f7e12b901c6baa777a3ad6a
ficsor <ficsor.lajos@gmail.com>
2022-04-03 16:00:37 +0200

    5. heti gyakorlat 1- feladat - (Eclipse verzió váltás)

--- teglalap/src/teglalap/RunTeglalap.java
+++ teglalap/src/teglalap/RunTeglalap.java
@@ -4,30 +4,28 @@ public class RunTeglalap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Teglalap a = new Teglalap(10,20);
-	Teglalap b = new Teglalap(10);
-	Teglalap c = a;
-	Teglalap d = new Teglalap(10,20);
-	
-	System.out.println("a= " + a);
-	System.out.println("b= " + b);
-	System.out.println("c= " + c);
-	
-	System.out.println(a==b);
-	System.out.println(a==c);
+		Teglalap a = new Teglalap(10,20);
+		Teglalap b = new Teglalap(10);
+		Teglalap c = a;
+		Teglalap d = new Teglalap(10,20);
 
-	if (a.teruletOsszehasonlito(b)) {
-		System.out.println("az a teglalap nagyobb");
-	} else {
-		System.out.println("a b teglalap nagyobb");
-	}
-	
-	if (a.egybevago(d)) {
-		System.out.println("a teglalapok egybevágóak");
-	} else {
-		System.out.println("a teglalapok nem egybevágóak");
-	}
+		System.out.println("a= " + a);
+		System.out.println("b= " + b);
+		System.out.println("c= " + c);
 
-	
-}
+		System.out.println(a==b);
+		System.out.println(a==c);
+
+		if (a.teruletOsszehasonlito(b)) {
+			System.out.println("az a teglalap nagyobb");
+		} else {
+			System.out.println("a b teglalap nagyobb");
+		}
+
+		if (a.egybevago(d)) {
+			System.out.println("a teglalapok egybevï¿½gï¿½ak");
+		} else {
+			System.out.println("a teglalapok nem egybevï¿½gï¿½ak");
+		}
+	}
 }
